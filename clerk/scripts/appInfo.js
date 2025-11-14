6baa5f01e4b631eb3ff4b77db243cc72a43320b0
Alex <59529933+shadowsystemss@users.noreply.github.com>
2025-11-14 19:52:31 +0300

    Update appInfo.js

--- clerk/scripts/appInfo.js
+++ clerk/scripts/appInfo.js
@@ -1,3 +1,14 @@
-log(Clerk.Version);
-log(Clerk.VersionName);
-log(Clerk.VersionId);
+clear();
+log(`Название: ${Clerk.VersionName}`);
+log(`AppId: ${Clerk.VersionId}`);
+log(`Ваша версия: ${Clerk.Version}`);
+changeMode();
+fetch(
+  "https://raw.githubusercontent.com/shadowsystemss/Update/refs/heads/main/clerk/version"
+)
+  .then((data) => {
+    log(`Актуальная версия: ${data}`);
+    if (data == Clerk.Version) log("У вас актуальная версия приложения.");
+    else log("У вас иная версия приложения.");
+  })
+  .catch(log);
