c25d9e0b4c121c8aeb3d8a3a32ff3f370890fe66
Alex <59529933+shadowsystemss@users.noreply.github.com>
2025-11-14 20:32:15 +0300

    Create setDefaultScript.js

---
+++ clerk/scripts/setDefaultScript.js
@@ -0,0 +1,15 @@
+// Set default script
+// Edited 14.11.2025
+// Tested on version 1.1.5 (MyBad!)
+(async () => {
+  clear();
+  let file = await FileSystem.PickFileAsync();
+  let path = Path.Combine(FileSystem.ScriptDirectory, "default");
+  if (!file) {
+    alert("Вы не выбрали скрипт");
+    return;
+  }
+  await File.CopyAsync(file, path, cancel);
+  Theme.BackgroundImage = path;
+  log("Скрипт по умолчанию выбран");
+})().catch(log);
